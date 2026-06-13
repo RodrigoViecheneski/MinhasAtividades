93df9fc33a8cc4f405937994bf43040df1f41b19
RodrigoViecheneski <rodrigoviecheneski@gmail.com>
2026-06-12 22:18:15 -0300

    secound commit

---
+++ operacoesAritimeticas2.py
@@ -0,0 +1,19 @@
+from math import sqrt
+a = int(input("a: "))
+
+dobro = a * 2
+triplo = a * 3
+quadrado = a ** 2
+raiz = a ** 0.5
+cubo = a ** 3
+raizQuadrada = sqrt(a)
+print("----------------------") 
+print(f"Sendo n = {a}, tem-se:")
+print(f"Dobro........: {dobro}")
+print(f"Triplo.......: {triplo}")
+print(f"Quadrado.....: {quadrado}")
+print(f"Raiz.........: {raiz}")
+print(f"Cubo.........: {cubo}")
+print(f"Raiz Quadrada: {raizQuadrada:.4f}")
+print("----------------------")
+
